dd45617c3b50e04baa83269ca8525bf3353d688f
yeasinopu17 <yeasinopu@gmail.com>
2021-12-13 16:57:24 +0600

    complete 257

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,7 +1,8 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = (props) => {
     const dispatch = useDispatch();
@@ -9,6 +10,8 @@ const ProductItem = (props) => {
     const { title, price, description, id } = props;
 
     const addToCartHandler = () => {
+        // and then send Http request
+        // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
         dispatch(
             cartActions.addItemToCart({
                 id,
@@ -17,6 +20,7 @@ const ProductItem = (props) => {
             })
         );
     };
+
     return (
         <li className={classes.item}>
             <Card>
